3768480ef3588d4d553413bbd556ecfc40f8b97b
Yeongmin Kim <oinochoe@naver.com>
2019-12-21 15:43:04 +0900

    assets 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'index.bundle.js'
+    path: path.join(__dirname, "/dist"),
+    filename: "index.bundle.js"
   },
   module: {
     rules: [
@@ -13,14 +14,28 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: "babel-loader"
         }
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: "./src/index.html"
+    }),
+    new WebpackPwaManifest({
+      name: "리액트 메모",
+      short_name: "메모",
+      description: "리액트로 제작한 메모앱입니다",
+      backgrond_color: "#ffffff",
+      crossorigin: "use-credentials", // 인증 사용하겠다.
+      theme_color: "#eeeeee",
+      icons: [
+        {
+          src: path.resolve("src/assets/icon.png"),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
     })
   ]
-}
+};
