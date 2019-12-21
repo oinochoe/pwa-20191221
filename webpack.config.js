f03582db535030d06f251bac62cc29118e7881fa
Yeongmin Kim <oinochoe@naver.com>
2019-12-21 15:59:32 +0900

    pwa perfect

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
+const { GenerateSW } = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -36,6 +37,9 @@ module.exports = {
           sizes: [96, 128, 192, 256, 384, 512]
         }
       ]
+    }),
+    new GenerateSW({
+      include: [/\.html$/, /\.js$/]
     })
   ]
 };
